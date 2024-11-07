b479d35436f4ff7f605e7a34d364204a141e0ebf
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-11-07 20:22:20 +0530

    Update prog13.py

--- prog13.py
+++ prog13.py
@@ -1,11 +1,9 @@
 # WAP to read a file and print the total number of characters, words, and lines
 
-def file_statistics(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        num_lines = len(lines)
-        num_words = sum(len(line.split()) for line in lines)
-        num_chars = sum(len(line) for line in lines)
-    print(num_chars, num_words, num_lines)
-
-file_statistics("abc.txt")
+filename=input("Enter filename:")
+file=open(filename, 'r') 
+lines = file.readlines()
+num_lines = len(lines)
+num_words = sum(len(line.split()) for line in lines)
+num_chars = sum(len(line) for line in lines)
+print(num_chars, num_words, num_lines)
