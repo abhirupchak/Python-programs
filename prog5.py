9b5bdd131b4b757327fbada142dfa2b5c3721d04
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-11-07 19:24:44 +0530

    Update prog5.py

--- prog5.py
+++ prog5.py
@@ -1,22 +1,11 @@
-# WAP tp create a pyramid of the character "*" and a revese pyramid 
+# WAP to create a pyramid of the character '*' and a reverse pyramid
 
 def pyramid(n):
-    for i in range(1,n+1):
-        for j in range(n-i):
-            print(" ", end=" ")
-        for k in range(1,i):
-            print("*", end=" ")
-        for l in range(2,i):
-            print("*",end=" ")
-        print()       
-    print()
-    for m in range(n,-1,-1):
-        for g in range(n-m):
-            print(" ", end=" ")
-        for o in range(1,m):
-            print("*", end=" ")
-        for p in range(2,m):
-            print("*",end=" ")
-        print()   
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "*" * (2 * i - 1))
 
-pyramid(5)
+def reverse_pyramid(n):
+    for i in range(n, 0, -1):
+        print(" " * (n - i) + "*" * (2 * i - 1))
+
+
