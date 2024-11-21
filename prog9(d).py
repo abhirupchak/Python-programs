fb1f5340aa5c28722f68b91d039b4278d9e07e95
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-11-21 19:42:50 +0530

    Create prog9(d).py

---
+++ prog9(d).py
@@ -0,0 +1,19 @@
+def copy_lines_to_files(input_file):
+    # Open the files for reading and writing
+    infile = open(input_file, 'r')
+    file1 = open('File1.txt', 'w')
+    file2 = open('File2.txt', 'w')
+    
+    line_number = 1  # Manually track line numbers
+    for line in infile:
+        if line_number % 2 == 0:
+            file1.write(line)
+        else:
+            file2.write(line)
+        line_number += 1  # Increment line number
+      
+    infile.close()
+    file1.close()
+    file2.close()
+
+    print("Lines copied successfully to File1.txt and File2.txt.")
