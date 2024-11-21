bbfa4e7c65a4e23b1819d9a4c00556ca286ccfbb
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-11-21 19:52:08 +0530

    Update prog11.py

--- prog11.py
+++ prog11.py
@@ -1,5 +1,6 @@
 def generate_cubes_dict():
     cubes_dict = {}
-    for i in range(1, 6):  # Iterate over the range
-        cubes_dict[i] = i ** 3  # Compute cube and assign to dictionary
+    for i in range(1, 6):
+        cubes_dict[i] = i ** 3
     print(cubes_dict)
+generate_cubes_dict()
