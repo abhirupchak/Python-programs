6c0ea9855bd230c0d95b7e9cbf009cf055e591aa
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-12-26 11:36:58 +0530

    Update prog9(c).py

--- prog9(c).py
+++ prog9(c).py
@@ -3,10 +3,5 @@
 def reverse_words_in_file(filename):
     with open(filename, 'r') as file:
         f=file.read()
-        s=""
-        for i in range(len(f),0,-1):
-            s=""
-            s+=f[i]
-        print(s)
-
+        print(f[::-1])
 reverse_words_in_file("textfile.txt")
