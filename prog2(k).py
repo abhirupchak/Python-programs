42ae8533219324ca0a94ef2cb8095124357abac4
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-12-26 10:34:26 +0530

    Update prog2(k).py

--- prog2(k).py
+++ prog2(k).py
@@ -1,20 +1,13 @@
 # 2.k. wap to generate prime numbers till n
 def isPrime(n):
 
-    if(n==1 or n==0):  return False
-  
+    if(n==1 or n==0):  
+        return False
     for i in range(2,n):
         if(n%i==0):
             return False
-  
-    return True
-
-
-
-
+  `  return True
 N = int(input("enter number till which prime numbers are to be generated:"))
-
 for i in range(1,N+1):
-  
-    if(isPrime(i)):
+  if(isPrime(i)):
         print(i)
