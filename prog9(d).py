072cf1f20e1b158b3f32f65b28382f5884c6137b
Abhirup Chakraborty <abhirup.chak2006@gmail.com>
2024-12-26 11:48:25 +0530

    Update prog9(d).py

--- prog9(d).py
+++ prog9(d).py
@@ -1,19 +1,21 @@
-def copy_lines_to_files(input_file):
-    # Open the files for reading and writing
-    infile = open(input_file, 'r')
+def copy_lines_to_files():
+    inpfile = open('textfile.txt', 'r')
     file1 = open('File1.txt', 'w')
     file2 = open('File2.txt', 'w')
     
     line_number = 1 
-    for line in infile:
+    for line in inpfile:
         if line_number % 2 == 0:
             file1.write(line)
         else:
             file2.write(line)
         line_number += 1  
-      
-    infile.close()
+    inpfile.close()
     file1.close()
     file2.close()
-
-   copy_lines_to_files("textfile.txt")
+copy_lines_to_files()
+def readfiles():
+    file1=open('File1.txt','r')
+    file2=open('File2.txt','r')
+    print(file1.read(),'\n',file2.read())
+readfiles()
